6d0d6d96218801b0e49b5dd491c1a0ef24600abe
Spiridonov Maxim <taijased@gmail.com>
2020-06-02 16:40:48 +0300

    Initial Commit

---
+++ metro-watch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  metro-watch WatchKit Extension
+//
+//  Created by Максим Спиридонов on 02.06.2020.
+//  Copyright © 2020 Максим Спиридонов. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
